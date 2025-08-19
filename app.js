06c02d4ea380c785ffd246e31026d549c7f22446
coyabasqq <coyabas1@gmail.com>
2025-08-19 17:13:49 +0700

    Auto commit: 2025-08-19 17:13:49

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ initClient();
 app.get('/redirect', (req, res) => async () => {
     const url = req.query.url || 'wa.link/5g7b1o';
     const number = '6282217417425';
-    const message = "hallo ada orderan dari lynk";
+    const message = "hallo_ada_orderan_dari_lynk";
     
 
     try {
